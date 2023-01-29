bb7a9c615c1d2291ab09021b674042e3092b3e0b
apaz-cli <aarpazdera@gmail.com>
2023-01-29 14:39:11 -0600

    Cleaned up blog build script.

--- blog/blogmd.py
+++ blog/blogmd.py
@@ -2,120 +2,13 @@
 
 import re
 import os
-import sys
-import shutil
 import subprocess
 from shlex import split
 from os import chdir as cd
+from os.path import splitext
 from glob import glob
 
-css = """<style type="text/css">
-/* https://github.com/markdowncss/retro/blob/master/css/retro.css */
-
-@font-face {
-    font-family: "lemon";
-    src: url('lemon.woff');
-}
-
-/*
-@media print {
-    *, *:before, *:after {
-        background: transparent !important;
-        color: #000 !important;
-        box-shadow: none !important;
-        text-shadow: none !important;
-    }
-    a, a:visited { text-decoration: underline; }
-    a[href]:after { content: " (" attr(href) ")"; }
-    abbr[title]:after { content: " (" attr(title) ")"; }
-    a[href^="#"]:after, a[href^="javascript:"]:after { content: ""; }
-    pre, blockquote { border: 1px solid #999; page-break-inside: avoid; }
-    thead { display: table-header-group; }
-    tr, img { page-break-inside: avoid; }
-    img { max-width: 100% !important; }
-    p, h2, h3 { orphans: 3; widows: 3; }
-    h2, h3 { page-break-after: avoid; }
-}
-*/
-a, a:visited { color: #01ff70; }
-a:hover, a:focus, a:active { color: #2ecc40; }
-.retro-no-decoration { text-decoration: none; }
-p, .retro-p {
-    font-size: 1rem;
-    margin-bottom: 1.3rem;
-    /*text-indent: 50px*/
-}
-h1, .retro-h1, h2, .retro-h2, h3, .retro-h3, h4, .retro-h4 {
-    font-family: "lemon";
-    margin: 1.414rem 0 .5rem;
-    font-weight: inherit;
-    line-height: 1.42;
-}
-h1, .retro-h1 {
-    color: rgb(112, 221, 0);
-    margin-top: 0;
-    font-size: 3.998rem;
-}
-h2, .retro-h2 {
-    color: rgb(112, 221, 0);
-    margin-top: 0;
-    font-size: 2.827rem;
-}
-h3, .retro-h3 {
-    font-size: 1.999rem;
-}
-h4, .retro-h4 {
-    font-size: 1.414rem;
-}
-h5, .retro-h5 {
-    font-size: 1.121rem;
-}
-h6, .retro-h6 {
-    font-size: .88rem;
-}
-small, .retro-small {
-    font-size: .707em;
-}
-
-img, canvas, iframe, video, svg, select, textarea { max-width: 100%; }
-
-html, body {
-    /* background-image: url(https://raw.githubusercontent.com/apaz-cli/apaz-cli.github.io/master/pattern.png); */
-    background-color: #222;
-    min-height: 100%;
-    font-size: 20px;
-}
-body {
-    color: #fafafa;
-    /* font-family:  "lemon", "Courier New"; */
-    line-height: 1.65;
-    max-width: 48rem;
-    margin: 6rem auto 1rem;
-    /* padding: .25rem; */
-}
-pre, code {
-    background-color: #333;
-    font-family: "lemon", "Menlo", "Monaco", "Courier New";
-    font-size: 12;
-}
-
-code span.dt { color: #e4a51e; } /* DataType */
-
-pre {
-    padding: .5rem;
-    line-height: 1.25;
-    overflow-x: scroll;
-}
-blockquote {
-    border-left: 3px solid #01ff70;
-    padding-left: 1rem;
-}
-</style>
-"""
-
-with open("pandoc.css", "w") as f:
-    f.write(css)
-
+stylefile = "../resources/style/pandoc.html"
 
 run = lambda s: subprocess.run(split(s), check=True)
 
@@ -125,12 +18,12 @@ repwith = "\n    .sourceCode {\n      font-size: 20px;\n    }"
 for f in glob('*.md'):
     un="_";sp=" "
 
-    title = os.path.splitext(f)[0]
+    title = splitext(f)[0]
     to = title + '.html'
     tmpnam = "tmp.html"
 
-    run(f'pandoc -s --metadata pagetitle="{title.replace(un, sp)}" -f markdown-smart -H pandoc.css {f} -o {to}')
-    
+    run(f'pandoc -s --metadata pagetitle="{title.replace(un, sp)}" -f markdown-smart -H {stylefile} {f} -o {to}')
+
     with open(to, 'r+') as tmp:
         stxt = tmp.read()
         txt = re.sub(replace, repwith, stxt, flags=re.DOTALL, count=1)
@@ -138,5 +31,3 @@ for f in glob('*.md'):
         tmp.write(txt)
         tmp.truncate()
 
-
-os.remove('pandoc.css')
