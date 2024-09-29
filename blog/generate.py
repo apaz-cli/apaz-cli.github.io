feda71f343d0347d30657b899b51420113c7e33b
apaz-cli <aarpazdera@gmail.com>
2024-09-28 19:31:36 -0500

    Added opengraph meta tags to all articles.

--- blog/generate.py
+++ blog/generate.py
@@ -20,24 +20,46 @@ run = lambda s: subprocess.run(split(s), check=True)
 replace = "\s+.sourceCode {\s+background-color: transparent;\s+overflow: visible;\s+}"
 repwith = "\n    .sourceCode {\n      font-size: 20px;\n    }"
 
+
 def generate_article(i, f):
-    un="_";sp=" "
+
+    def replace_meta_with_opengraph(html):
+        meta_rep = "  <!-- meta -->\n"
+        titlegroup = re.search("<h1.*>(.*?)</h1>", html)
+        subtitlegroup = re.search("<h4.*>(.*?)</h4>", html)
+        first_image = re.search('<img.*src="(.*?)".*>', html)
+
+        meta_with = ""
+        if titlegroup:
+            meta_with += f"<meta name=\"og:title\" content=\"{titlegroup.group(1) if titlegroup else ''}\">\n"
+            if subtitlegroup:
+                meta_with += f"  <meta name=\"og:description\" content=\"{subtitlegroup.group(1) if subtitlegroup else ''}\">\n"
+            if first_image:
+                meta_with += f"  <meta name=\"og:image\" content=\"{first_image.group(1) if first_image else ''}\">\n"
+        return html.replace(meta_rep, meta_with)
 
     title = splitext(f)[0]
-    to = title + '.html'
+    to = title + ".html"
     unstyled = title + "-unstyled.html"
 
-    run(f'pandoc -s --metadata pagetitle="{title.replace(un, sp)}" -f markdown-smart -H {stylefile} {f} -o {to}')
-    run(f'pandoc -s --metadata pagetitle="{title.replace(un, sp)}" -f markdown-smart {f} -o {unstyled}')
+    un = "_"
+    sp = " "
+    run(
+        f'pandoc -s --metadata pagetitle="{title.replace(un, sp)}" -f markdown-smart -H {stylefile} {f} -o {to}'
+    )
+    run(
+        f'pandoc -s --metadata pagetitle="{title.replace(un, sp)}" -f markdown-smart {f} -o {unstyled}'
+    )
 
-    with open(to, 'r+') as tmp:
+    with open(to, "r+") as tmp:
         stxt = tmp.read()
         txt = re.sub(replace, repwith, stxt, flags=re.DOTALL, count=1)
+        txt = replace_meta_with_opengraph(txt)
         tmp.seek(0)
         tmp.write(txt)
         tmp.truncate()
 
-    with open(unstyled, 'r+') as tmp:
+    with open(unstyled, "r+") as tmp:
         stxt = tmp.read()
         txt = re.sub("\s+<style>.*</style>", "", stxt, flags=re.DOTALL, count=1)
         tmp.seek(0)
@@ -46,7 +68,8 @@ def generate_article(i, f):
 
     return i, f
 
-md_files = list(enumerate(glob('*.md'), 1))
+
+md_files = list(enumerate(glob("*.md"), 1))
 if only:
     md_files = [(i, f) for i, f in md_files if i == int(only)]
 
@@ -54,9 +77,10 @@ with ThreadPoolExecutor() as executor:
     futures = {executor.submit(generate_article, i, f): (i, f) for i, f in md_files}
 
     while futures:
-        done, _ = concurrent.futures.wait(futures, timeout=0.1, return_when=concurrent.futures.FIRST_COMPLETED)
+        done, _ = concurrent.futures.wait(
+            futures, timeout=0.1, return_when=concurrent.futures.FIRST_COMPLETED
+        )
         for future in done:
             i, f = future.result()
             print(f"Generated article {i}: {f}")
             futures.pop(future)
-
