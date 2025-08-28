328050b75398935e914b8976d1378f33596a8ab2
apaz-cli <aarpazdera@gmail.com>
2025-08-27 21:57:36 -0500

    fix: set html and body backgrounds to transparent for translucent effect

--- reading/generate.py
+++ reading/generate.py
@@ -190,10 +190,10 @@ def generate_html(items: List[ReadingItem]):
             margin: 6rem auto 1rem;
             padding: .25rem;
             max-width: 90rem;
-            background-color: #222;
+            background-color: transparent;
         }}
         html {{
-            background-color: #222;
+            background-color: transparent;
         }}
         body::before {{
             content: '';
