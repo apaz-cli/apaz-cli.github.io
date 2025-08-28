b0807ff0a5394d044bbdeb88ac6cd3b6b6395c33
apaz-cli <aarpazdera@gmail.com>
2025-08-27 23:58:16 -0500

    μ

--- reading/generate.py
+++ reading/generate.py
@@ -145,6 +145,9 @@ def get_info_from_url(url: str | None) -> tuple[Optional[str], Optional[str]]:
             abstract = abstract_match.group(1).strip()
             abstract = re.sub(r'\s+', ' ', abstract)
 
+        title = title.replace("$\mu$", "μ")
+        abstract = abstract.replace("$\mu$", "μ")
+
         return title, abstract
 
     except Exception as e:
