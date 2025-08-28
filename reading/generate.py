41754c5bb03563b5867b6156b494700d8653e688
apaz-cli <aarpazdera@gmail.com>
2025-08-27 22:06:12 -0500

    style: Update h2 color to #01ff70 in generate.py

--- reading/generate.py
+++ reading/generate.py
@@ -257,7 +257,7 @@ def generate_html(items: List[ReadingItem]):
             }}
         }}
         h2 {{
-            color: #fafafa;
+            color: #01ff70;
         }}
         h3 {{
             font-family: "lemon";
